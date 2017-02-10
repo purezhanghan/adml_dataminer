8682aa6dd7ad58fb732d20896734c9613c3932d0
ningningliu <lnn35@hotmail.com>
2017-02-10 13:28:30 +0000

    remove variable with more than 90% missing data and split dataset into explanatory variables and responese variable

--- ningning/crunch_data.py
+++ ningning/crunch_data.py
@@ -13,7 +13,21 @@ df_missing_data.columns = ['counts']
 df_missing_data= df_missing_data[df_missing_data.counts != 0]
 # Calculate missing percentage, we should delete the variables with 90% missing value
 df_missing_data['missing_percent'] = df_missing_data.counts/total_data
-print(df_missing_data)
+# print(df_missing_data)
+
+# Take out target value
+target = train.Response.values
+print(len(target))
+
+# Reduce dimension: Identity highly irrelevant variables
+# Id, Medical_History_10 (99.1% missing), Medical_History_24(93.6% missing), Medical_History_32 (98.1% missing)
+# Remove target value 'Response'
+train.drop(['Id','Response','Medical_History_10','Medical_History_24','Medical_History_32'], axis =1, inplace = True)
+print(train.columns)
+
+
+
+
 
 
 
