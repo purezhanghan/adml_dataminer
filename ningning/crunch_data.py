f09df24cfd2e5bee4f23ffda91d120c6e7f9e9f0
ningningliu <lnn35@hotmail.com>
2017-02-10 12:16:20 +0000

    use pd.read_csv so no need to transfer data type

--- ningning/crunch_data.py
+++ ningning/crunch_data.py
@@ -1,6 +1,6 @@
-from load_data import df
-import pandas as pd
-import numpy as np
+# from load_data import df
+# import pandas as pd
+# import numpy as np
 # print(df.columns)
 # print(df.describe())
 # print(df.dtypes)
@@ -18,9 +18,9 @@ import numpy as np
 
 
 # Transfer data type object to categorical
-def transfer_dtype(variable):
-    df[variable]= df[variable].astype('category')
-    print(df[variable].dtypes)
+# def transfer_dtype(variable):
+#     df[variable]= df[variable].astype('category')
+#     print(df[variable].dtypes)
 
 
 # transfer_dtype('Product_Info_1')
@@ -29,29 +29,29 @@ def transfer_dtype(variable):
 
 
 # Transfer data type to continuous
-def transfer_dtype_continuous(variable):
-    df[variable]= pd.to_numeric(df[variable], errors='coerce')
-    print(df[variable].dtypes)
+# def transfer_dtype_continuous(variable):
+#     df[variable]= pd.to_numeric(df[variable], errors='coerce')
+#     print(df[variable].dtypes)
 
 
 # Create list of variables
-cont_variable_list = ['Product_Info_4', 'Ins_Age','Ht','Wt','BMI','Employment_Info_1','Employment_Info_4',
-                      'Employment_Info_6','Insurance_History_5', 'Family_Hist_2', 'Family_Hist_3', 'Family_Hist_4', 'Family_Hist_5']
+# cont_variable_list = ['Product_Info_4', 'Ins_Age','Ht','Wt','BMI','Employment_Info_1','Employment_Info_4',
+#                       'Employment_Info_6','Insurance_History_5', 'Family_Hist_2', 'Family_Hist_3', 'Family_Hist_4', 'Family_Hist_5']
 
-dis_variable_list =['Medical_History_1', 'Medical_History_10', 'Medical_History_15', 'Medical_History_24', 'Medical_History_32']
+# dis_variable_list =['Medical_History_1', 'Medical_History_10', 'Medical_History_15', 'Medical_History_24', 'Medical_History_32']
 
-for i in range (48):
-    i += 1
-    print(i)
-    dis_variable_list.append('Medical_Keyword_'+'i')
+# for i in range (48):
+#     i += 1
+#     print(i)
+#     dis_variable_list.append('Medical_Keyword_'+'i')
 # print(len(dis_variable_list))
 
-cat_variable_list =[]
-for header in df.columns:
-    if header in cont_variable_list and dis_variable_list:
-        pass
-    else:
-        cat_variable_list.append(header)
+# cat_variable_list =[]
+# for header in df.columns:
+#     if header in cont_variable_list and dis_variable_list:
+#         pass
+#     else:
+#         cat_variable_list.append(header)
 
 
 # Transfer to continuous data
--- ningning/load_data.py
+++ ningning/load_data.py
@@ -1,31 +1,12 @@
-import csv as csv
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-file = open('train.csv')
-file_csv = csv.reader(file)
 
-# print(file_csv)
+# Load data file
+train = pd.read_csv('train.csv')
+print(train.dtypes)
+# print(train.Product_Info_2.str[0])
 
-data =[]
-for row in file_csv:
-    data.append(row)
-
-# print(pprint.pprint(data [2]))
-
-header= data[0]
-data.pop(0)
-
-df= pd.DataFrame(data,columns= header)
-# print(df)
-# print(len(data))
-# print(df.head())
-# print(df.columns)
+test = pd.read_csv('test.csv')
+print(test.dtypes)
 
 
-if __name__ == "__main__":
-    print("Run as a script")
 
-
-arrary = df.values
-scaled_arrary= StandardScaler().fit_transform(arrary)
-print(scaled_arrary[:3,:5])
