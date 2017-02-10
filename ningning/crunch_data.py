37b7ad948fd3aa8e39578bafc074cae4b9afd3ee
ningningliu <lnn35@hotmail.com>
2017-02-10 12:59:14 +0000

    display missing variable with its missing percentage, take note of variables with 90% more missing data

--- ningning/crunch_data.py
+++ ningning/crunch_data.py
@@ -9,9 +9,13 @@ missing_data = train.isnull().sum()
 total_data  = len(train)
 df_missing_data = missing_data.to_frame()
 df_missing_data.columns = ['counts']
+# Identify missing categories
 df_missing_data= df_missing_data[df_missing_data.counts != 0]
+# Calculate missing percentage, we should delete the variables with 90% missing value
 df_missing_data['missing_percent'] = df_missing_data.counts/total_data
 print(df_missing_data)
+
+
 
 
 
