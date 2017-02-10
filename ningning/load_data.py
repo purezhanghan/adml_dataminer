d2fa815b0b3f8ed89f90656e1737100a28c0fd87
ningningliu <lnn35@hotmail.com>
2017-02-10 14:52:43 +0000

    refined data loader

--- ningning/load_data.py
+++ ningning/load_data.py
@@ -1,14 +1,21 @@
 import pandas as pd
 
 # Load data file
-train = pd.read_csv('train.csv')
-print(train.dtypes)
+# train = pd.read_csv('train.csv')
+# print(train.dtypes)
 # print(train.Product_Info_2.str[0])
 
-test = pd.read_csv('test.csv')
-print(test.dtypes)
+# test = pd.read_csv('test.csv')
+# print(test.dtypes)
 
+def data_loader(path):
+    file = pd.read_csv(path)
+    return file
 
+train= data_loader('train.csv')
+# print(train)
+test= data_loader('test.csv')
+# print(test)
 
 
 
