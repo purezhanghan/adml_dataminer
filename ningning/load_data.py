d2aad36fad263d4f67114f4d3300d40d8b96d939
ningningliu <lnn35@hotmail.com>
2017-02-10 12:55:27 +0000

    Load data properly

--- ningning/load_data.py
+++ ningning/load_data.py
@@ -2,11 +2,11 @@ import pandas as pd
 
 # Load data file
 train = pd.read_csv('train.csv')
-# print(train.dtypes)
+print(train.dtypes)
 # print(train.Product_Info_2.str[0])
 
 test = pd.read_csv('test.csv')
-# print(test.dtypes)
+print(test.dtypes)
 
 
 
