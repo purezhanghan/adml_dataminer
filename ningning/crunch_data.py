174fc960d042efe0f7a8f3345b6c43fcccf23f45
ningningliu <lnn35@hotmail.com>
2017-02-11 16:09:50 +0000

    test my git branch commits

--- ningning/crunch_data.py
+++ ningning/crunch_data.py
@@ -1,10 +1,10 @@
 from load_data import train
-import pandas as pd
-import numpy as np
+# import pandas as pd
+# import numpy as np
 
 # Explore missing data
 missing_data = train.isnull().sum()
-# print(missing_data.dtype)
+# print(missing_data.dtypes)
 # print(type(missing_data))
 total_data  = len(train)
 df_missing_data = missing_data.to_frame()
