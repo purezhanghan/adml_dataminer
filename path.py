850ee9fa63a3b5caa0e52b2f7584def8ea895393
Entheos1994 <pete.zhanghan@yahoo.com>
2017-05-12 22:35:48 +0100

    data cleanup

--- path.py
+++ path.py
@@ -11,8 +11,8 @@ TRAIN_PATH = os.path.join(PROJECT_PATH, "train.csv")
 TEST_PATH = os.path.join(PROJECT_PATH, "test.csv")
 SUBMISSION_PATH = os.path.join(PROJECT_PATH, "submission.csv")
 
-TRAIN = os.path.join(PROJECT_PATH, "train.csv")
-TEST = os.path.join(PROJECT_PATH, "test.csv")
+PARSED_TRAIN = os.path.join(PROJECT_PATH, "parsed_train.csv")
+PARSED_TEST = os.path.join(PROJECT_PATH, "parsed_test.csv")
 
-TRAIN_NORMAL = os.path.join(PROJECT_PATH, "train_normal.csv")
-TEST_NORMAL = os.path.join(PROJECT_PATH, "test_normal.csv")
+TRAIN_NORMAL = os.path.join(PROJECT_PATH, "parsed_train_normalized.csv")
+TEST_NORMAL = os.path.join(PROJECT_PATH, "parsed_test_normalized.csv")
